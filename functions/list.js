885c446e2d643d4016c13d0d1293801e8d22e03f
Liu Dapeng <verydapeng@gmail.com>
2023-08-20 18:46:10 +0800

    Test

--- functions/list.js
+++ functions/list.js
@@ -1,10 +1,10 @@
 export async function onRequest(context) {
-    let keys = (await context.env.KOPI.list()).keys;
-    let v = {};
+    // let keys = (await context.env.KOPI.list()).keys;
+    // let v = {};
 
-    for (let { name } of keys) {
-        v[name] = await env.KOPI.get(name);
-    }
+    // for (let { name } of keys) {
+    //     v[name] = await env.KOPI.get(name);
+    // }
 
-    return new Response(JSON.stringify(v), { headers: { "Content-Type": "application/json" } });
+    return new Response(JSON.stringify(context));
 }
