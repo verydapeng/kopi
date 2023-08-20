ac44206cedc702da6fa032b6a2d959bcf9ef4315
Liu Dapeng <verydapeng@gmail.com>
2023-08-20 21:41:24 +0800

    Test

--- functions/record.js
+++ functions/record.js
@@ -1,4 +1,5 @@
-export async function onRequest({ env }) {
+export async function onRequest({ request, env }) {
+    let url = request.url;
     await env.KOPI.put(url.searchParams.get("name"), url.searchParams.get("drink"), { expirationTtl: 14400 });
     return new Response("DONE");
 }
