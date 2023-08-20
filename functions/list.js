d0343010c6becf2da9fd1ed839a58230072356e2
Liu Dapeng <verydapeng@gmail.com>
2023-08-20 18:31:28 +0800

    Add list order function

--- functions/list.js
+++ functions/list.js
@@ -1,3 +1,10 @@
-export function onRequest(context) {
-    return new Response("Hello, world!")
+export async function onRequest(context) {
+    let keys = (await env.KOPI.list()).keys;
+    let v = {};
+
+    for (let { name } of keys) {
+        v[name] = await env.KOPI.get(name);
+    }
+
+    return new Response(JSON.stringify(v), { headers: { "Content-Type": "application/json" } });
 }
