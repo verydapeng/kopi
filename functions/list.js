bfa8b4359ebb3aeeb549b01ec4ceeac4d70c38c4
Liu Dapeng <verydapeng@gmail.com>
2023-08-24 00:33:08 +0800

    Beautify

--- functions/list.js
+++ functions/list.js
@@ -1,12 +1,11 @@
 export async function onRequest({ env }) {
-    let keys = (await env.KOPI.list()).keys;
-
-    let response = Promise.all(keys.map(async name => {
-        return {
+    let response = [];    
+    for (let name of (await env.KOPI.list()).keys) {
+        response.push({
             name,
-            drink: (await env.KOPI.get(name))
-        }
-    }));
+            drink: await env.KOPI.get(name)
+        });
+    }
 
     return new Response(JSON.stringify(response), { headers: { "Content-Type": "application/json" } });
 }
